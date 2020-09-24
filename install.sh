3ce4c00c6b2dc7d6d4bb3f9fbcf202c59d6f4ea6
Muhammad bin Yusrat <ibnyusrat@gmail.com>
2020-09-24 18:16:15 +0500

    Updated install script to copy .ideavimrc as well.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/
+cp .ideavimrc ~/
 vim +'PlugInstall --sync' +qa
 vim +'CocInstall coc-json coc-tsserver' +qa
-mkdir ~/.fonts
+mkdir -p ~/.fonts
 cp "Cousine Regular Nerd Font Complete.ttf" ~/.fonts/
